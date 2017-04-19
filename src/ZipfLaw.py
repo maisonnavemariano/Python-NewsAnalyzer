bc962be5f9c10ec815c8ea7f7da38c4ca53be631
mmaisonnave <maisonnavemariano@gmail.com>
2017-04-19 17:04:46 -0300

    Clase documento agregada. Histograma agregado
    Merge branch 'master' of https://github.com/maisonnavemariano/Python-NewsAnalyzer
    
    Conflicts:
    	stopwords/myStopList0.1.txt
    	stopwords/myStopList0.25.txt
    	stopwords/myStopList0.5.txt
    	stopwords/myStopList0.8.txt
    	stopwords/tmp

--- src/ZipfLaw.py
+++ src/ZipfLaw.py
@@ -4,8 +4,8 @@
 import math
 
 # INPUTs
-cant_instancias_mayor_cluster = 5 #121
-cant_instancias               = 100 #1689
+cant_instancias_mayor_cluster = 300 #121
+cant_instancias               = 1689 #1689
 
 instancias_acumuladas = 0
 nro_cluster = 0
@@ -23,6 +23,7 @@ while instancias_acumuladas < cant_instancias:
 #    sum = sum + valores[i]
 #print("primer 20%: "+str(sum))
 
+print(" * *  * *  * *  * *  * *  Respetando la distribucion * *  * *  * *  * *  * * ")
 print("suma: "+string_suma[:-3])
 print("instancias acumuladas: "+str(instancias_acumuladas))
 print("cantidad de clusters necesarios: "+str(nro_cluster))
@@ -41,6 +42,28 @@ while(instancias_acumuladas < cant_instancias80_porciento):
     instancias_acumuladas = instancias_acumuladas + valores[-1]
 
 cant_total_clusters = cant_instancias - cant_instancias80_porciento +nro_cluster
+print(" * *  * *  * *  * *  * *  Respetando el 80-20  * *  * *  * *  * *  * * ")
 print("cantidad de clusters para cubrir el 80% de las instancias (1351): "+ str(nro_cluster))
 print("cantidad de clusters total: "+str(cant_total_clusters))
 print("cantidad de clusters teoricos: "+str(nro_cluster/0.2))
+
+
+cant_instancias_mayor_cluster = 50
+for cant_instancias_mayor_cluster in range (10,500):
+    print("frec mas alta: "+str(cant_instancias_mayor_cluster))
+    instancias_acumuladas = 0
+    nro_cluster = 0
+    while instancias_acumuladas < cant_instancias:
+        nro_cluster = nro_cluster + 1
+        instancias_acumuladas = instancias_acumuladas + math.ceil((cant_instancias_mayor_cluster / nro_cluster))
+
+
+    instancias_acumuladas = 0
+    nro_cluster_necesarios = nro_cluster
+    nro_cluster = 0
+    while (instancias_acumuladas < cant_instancias80_porciento):
+        nro_cluster = nro_cluster + 1
+        instancias_acumuladas = instancias_acumuladas + math.ceil((cant_instancias_mayor_cluster / nro_cluster))
+    nro_cluster_teorico = nro_cluster/0.2
+    print("clusters necesarios: "+str(nro_cluster_necesarios))
+    print("clusters teoricos: "+str(nro_cluster_teorico))
